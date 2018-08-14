818347cbaf729bdc34f58972635ba281ffbe0c68
SandraND <lainfaith2@gmail.com>
2018-08-14 12:17:22 +0200

    group model

---
+++ models/group.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const groupSchema = new Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectID, ref: 'User' , 
+    },
+    groupname: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    numplayers: {
+        type: Number,
+        required: true
+    },
+    numpoints: {
+        type: Number
+    },
+    gametype: {
+        type: String
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    game: {
+        type: String,
+        enum: ['Warhammer40k', 'Magic', 'X-Wing'],
+        required: true
+    },
+    players: [{
+        type: mongoose.Schema.Types.ObjectID, ref: 'User' , 
+    }]
+}, {
+    timestamps: true
+});
+
+const Group = mongoose.model('Group', groupSchema);
+
+module.exports = Group;
