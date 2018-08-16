ef80b165f5cb78d8d7e17290d7d45ef93cabeb98
SandraND <lainfaith2@gmail.com>
2018-08-16 11:40:48 +0200

    solve problem

--- models/group.js
+++ models/group.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const ObjectID = Schema.Types.ObjectId;
 
 
 const groupSchema = new Schema({
-    // owner: {
-    //     type: mongoose.Schema.Types.ObjectID, ref: 'User' , 
-    // },
+    owner: {
+        type: ObjectID, 
+        ref: 'User'
+    },
     groupname: {
         type: String,
         required: true,
